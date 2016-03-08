6ac012d6bf53784a4e31881edd69bd3fb893542c
Aaron Tumulak <agtumulak@gmail.com>
2016-03-08 01:30:07 -0500

    Updated sn-solver with reference problem

--- src/sn-solver.cpp
+++ src/sn-solver.cpp
@@ -19,66 +19,66 @@ int main()
     settings_1.WriteKTol( 1.0e-5 );
     settings_1.WriteSclFluxTol( 1.0e-5 );
     settings_1.WriteSeed( 10 );
-    settings_1.WriteProgressPeriod( 100 );
+    settings_1.WriteProgressPeriod( 1 );
     settings_1.WriteWrongness( 100.0 );
 
-    // Create moderator material mat_1 //
-    Material mat_1;
+    // Create moderator material reflector //
+    Material reflector;
 
     // Absorption
-    mat_1.WriteMacroAbsXsec( 0.1, 0.05 );
-    mat_1.WriteMacroAbsXsec( 14.0, 0.0 );
+    reflector.WriteMacroAbsXsec( 0.1, 0.05 );
+    reflector.WriteMacroAbsXsec( 14.0, 0.025 );
 
     // Scattering
-    mat_1.WriteMacroScatXsec( 0.1, 0.1, 0.05 );
-    mat_1.WriteMacroScatXsec( 0.1, 14.0, 0.0 );
-    mat_1.WriteMacroScatXsec( 14.0, 0.1, 0.1 );
-    mat_1.WriteMacroScatXsec( 14.0, 14.0, 0.0 );
+    reflector.WriteMacroScatXsec( 0.1, 0.1, 0.25 );
+    reflector.WriteMacroScatXsec( 0.1, 14.0, 0.0 );
+    reflector.WriteMacroScatXsec( 14.0, 0.1, 0.1125 );
+    reflector.WriteMacroScatXsec( 14.0, 14.0, 0.1125 );
 
     // Fission
-    mat_1.WriteMacroFissXsec( 0.1, 0.0 );
-    mat_1.WriteMacroFissXsec( 14.0, 0.0 );
-    mat_1.WriteFissNu( 0.1, 1.0 );
-    mat_1.WriteFissNu( 14.0, 1.0 );
-    mat_1.WriteFissChi( 0.1, 0.0 );
-    mat_1.WriteFissChi( 14.0, 1.0 );
+    reflector.WriteMacroFissXsec( 0.1, 0.0 );
+    reflector.WriteMacroFissXsec( 14.0, 0.0 );
+    reflector.WriteFissNu( 0.1, 1.0 );
+    reflector.WriteFissNu( 14.0, 1.0 );
+    reflector.WriteFissChi( 0.1, 0.0 );
+    reflector.WriteFissChi( 14.0, 1.0 );
 
     // External source
-    mat_1.WriteExtSource( 0.1, 0.1 );
-    mat_1.WriteExtSource( 14.0, 0.0 );
+    reflector.WriteExtSource( 0.1, 0.0 );
+    reflector.WriteExtSource( 14.0, 0.0 );
 
-    // Create fissile material mat_2 //
-    Material mat_2;
+    // Create fissile material core //
+    Material core;
 
     // Absorption
-    mat_2.WriteMacroAbsXsec( 0.1, 0.0 );
-    mat_2.WriteMacroAbsXsec( 14.0, 0.1 );
+    core.WriteMacroAbsXsec( 0.1, 1.0 );
+    core.WriteMacroAbsXsec( 14.0, 0.075 );
 
     // Scattering
-    mat_2.WriteMacroScatXsec( 0.1, 0.1, 0.05 );
-    mat_2.WriteMacroScatXsec( 0.1, 14.0, 0.0 );
-    mat_2.WriteMacroScatXsec( 14.0, 0.1, 0.0 );
-    mat_2.WriteMacroScatXsec( 14.0, 14.0, 0.0 );
+    core.WriteMacroScatXsec( 0.1, 0.1, 1.0 );
+    core.WriteMacroScatXsec( 0.1, 14.0, 0.0 );
+    core.WriteMacroScatXsec( 14.0, 0.1, 0.001 );
+    core.WriteMacroScatXsec( 14.0, 14.0, 0.049 );
 
     // Fission
-    mat_2.WriteMacroFissXsec( 0.1, 0.05 );
-    mat_2.WriteMacroFissXsec( 14.0, 0.0 );
-    mat_2.WriteFissNu( 0.1, 1.0 );
-    mat_2.WriteFissNu( 14.0, 1.0 );
-    mat_2.WriteFissChi( 0.1, 0.0 );
-    mat_2.WriteFissChi( 14.0, 1.0 );
+    core.WriteMacroFissXsec( 0.1, 6.0 );
+    core.WriteMacroFissXsec( 14.0, 0.05 );
+    core.WriteFissNu( 0.1, 2.5 );
+    core.WriteFissNu( 14.0, 2.8 );
+    core.WriteFissChi( 0.1, 0.0 );
+    core.WriteFissChi( 14.0, 1.0 );
 
     // External source
-    mat_2.WriteExtSource( 0.1, 0.1 );
-    mat_2.WriteExtSource( 14.0, 0.0 );
+    core.WriteExtSource( 0.1, 0.0 );
+    core.WriteExtSource( 14.0, 0.0 );
 
     // Create layout //
     Layout layout_1;
-    layout_1.AddToEnd( mat_1, 50.0, 500, 1.0 );
-    layout_1.AddToEnd( mat_2, 50.0, 500, 1.0 );
+    layout_1.AddToEnd( reflector, 25.0, 1000, 1.0 );
+    layout_1.AddToEnd( core, 30.0, 1000, 1.0 );
 
     // Create slab and run //
     Slab slab_1( settings_1, layout_1 );
-    slab_1.Solve();
+    slab_1.EigenvalueSolve();
     slab_1.PrintScalarFluxes();
 }
