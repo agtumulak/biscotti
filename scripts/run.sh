80ec33b84e598b1d54692df5b6017bb895a11889
Aaron Tumulak <agtumulak@gmail.com>
2016-04-26 17:30:30 -0400

    Updated run script to choose main file

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,6 +3,11 @@
 PROJECT_DIR='/Users/atumulak/Developer/sn-solver'
 
 cd $PROJECT_DIR
+
+rm src/sn-solver.cpp
+cp $1 src/sn-solver.cpp
+
 make
-bin/sn-solver | tee -a $1
-python scripts/process.py $1
+
+bin/sn-solver | tee -a $2
+python scripts/process.py $2
